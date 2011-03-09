bdbead6372eca9989418fb1e728c257f70d555ad
Russell Holmes <russellfholmes@gmail.com>
2011-03-09 13:25:38 -0500

    fixed bug in knitkit migration

---
+++ engines/knitkit/db/data_migrations/ignore/20110127011152_create_desktop_app_knitkit.rb
@@ -0,0 +1,22 @@
+class CreateDesktopAppKnitkit
+  def self.up
+    app = DesktopApplication.create(
+      :description => 'KnitKit',
+      :icon => 'icon-globe',
+      :javascript_class_name => 'Compass.ErpApp.Desktop.Applications.Knitkit',
+      :internal_identifier => 'knitkit',
+      :shortcut_id => 'knitkit-win'
+    )
+    
+    PreferenceType.iid('desktop_shortcut').preferenced_records << app
+    PreferenceType.iid('autoload_application').preferenced_records << app
+
+    app.save
+
+    Role.create(:description => 'Knitkit Website', :internal_identifier => 'knitkit_website')
+  end
+
+  def self.down
+    DesktopApplication.destroy_all(:conditions => ['internal_identifier = ?','knitkit'])
+  end
+end
