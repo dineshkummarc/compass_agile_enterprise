9ceaf9cd3d7992062970be634c61bb07b4e03d94
Russell Holmes <russellfholmes@gmail.com>
2011-03-09 17:30:30 -0800

    Added exception handling for initial migration run

--- engines/knitkit/app/observers/knitkit_user_observer.rb
+++ engines/knitkit/app/observers/knitkit_user_observer.rb
@@ -2,6 +2,7 @@ class KnitkitUserObserver < ActiveRecord::Observer
   observe :user
 
   def after_create(user)
+    begin
     desktop = ::Desktop.create
     desktop.user = user
     #make sure to setup default preferences
@@ -11,5 +12,7 @@ class KnitkitUserObserver < ActiveRecord::Observer
     organizer.user = user
     #make sure to setup default preferences
     organizer.setup_default_preferences
+    rescue
+    end
   end
 end
