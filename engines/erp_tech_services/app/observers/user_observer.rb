77ee4220ffcfe3415697fa8b83c3bbcbf8ca4430
Russell Holmes <russellfholmes@gmail.com>
2011-03-15 10:49:17 -0400

    extensions to active_ext and minor bug fixes

--- engines/erp_tech_services/app/observers/user_observer.rb
+++
@@ -1,7 +0,0 @@
-class UserObserver < ActiveRecord::Observer
-  def after_create(user)
-    individual = Individual.create(:current_first_name => user.first_name, :current_last_name => user.last_name)
-    individual.party.user = user
-    individual.party.save
-  end
-end
