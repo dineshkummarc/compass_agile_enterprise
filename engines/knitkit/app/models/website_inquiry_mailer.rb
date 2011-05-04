3442c208c92ed67aceac4b294295b9185f7bc595
Adam Hull <adam@adam.olcc.lan>
2011-05-04 09:20:47 -0400

    fixing from email address

--- engines/knitkit/app/models/website_inquiry_mailer.rb
+++ engines/knitkit/app/models/website_inquiry_mailer.rb
@@ -2,7 +2,7 @@ class WebsiteInquiryMailer < ActionMailer::Base
 
   def inquiry(website_inquiry)
     @recipients  = website_inquiry.website.email
-    @from        = website_inquiry.website.email
+    @from        = website_inquiry.email
     @sent_on     = Time.now
     @subject     = "#{website_inquiry.website.title} Inquiry"
     @body        = {
