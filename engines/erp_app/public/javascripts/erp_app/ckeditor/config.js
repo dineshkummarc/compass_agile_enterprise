e830c393ef3264b14aad31b8dabb1711bf6cd171
root <root@monty.terralab.com>
2011-05-04 00:54:39 +0000

    adding jwplayer plugin to ckeditor

--- engines/erp_app/public/javascripts/erp_app/ckeditor/config.js
+++ engines/erp_app/public/javascripts/erp_app/ckeditor/config.js
@@ -8,5 +8,5 @@ CKEDITOR.editorConfig = function( config )
     // Define changes to default configuration here. For example:
     // config.language = 'fr';
     // config.uiColor = '#AADC6E';
-    config.extraPlugins = 'compasssave,compassupload';
+    config.extraPlugins = 'compasssave,compassupload,jwplayer';
 };
