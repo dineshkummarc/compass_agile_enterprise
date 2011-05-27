3e7c502496709ccade91f1c31710211d1541cce4
Adam H <adam@adam.olcc.lan>
2011-05-27 10:17:44 -0400

    moving observer loading to erp_search

--- engines/erp_base_erp_svcs/init.rb
+++ engines/erp_base_erp_svcs/init.rb
@@ -2,15 +2,6 @@ require 'erp_services/acts_as_erp_type'
 require 'erp_services/category/acts_as_category'
 require 'erp_services/is_describable'
 
-#add the observers path to load_paths
-ActiveSupport::Dependencies.load_paths << RAILS_ROOT + "#{File.dirname(__FILE__)}/app/observers"
-ActiveRecord::Base.observers << :contact_observer
-#ActiveRecord::Base.observers << :solr_email_address_observer
-ActiveRecord::Base.observers << :individual_observer
-ActiveRecord::Base.observers << :organization_observer
-ActiveRecord::Base.observers << :party_observer
-ActiveRecord::Base.observers << :postal_address_observer
-
 # remove plugin from load_once_paths
 ActiveSupport::Dependencies.load_once_paths -= ActiveSupport::Dependencies.load_once_paths.select{|path| path =~ %r(^#{File.dirname(__FILE__)}) }
 
--- engines/erp_search/init.rb
+++ engines/erp_search/init.rb
@@ -1,6 +1,15 @@
 $USE_SOLR_FOR_CONTENT = true
 require 'sunspot_rails'
 
+#add the observers path to load_paths
+ActiveSupport::Dependencies.load_paths << RAILS_ROOT + "#{File.dirname(__FILE__)}/app/observers"
+ActiveRecord::Base.observers << :contact_observer
+#ActiveRecord::Base.observers << :solr_email_address_observer
+ActiveRecord::Base.observers << :individual_observer
+ActiveRecord::Base.observers << :organization_observer
+ActiveRecord::Base.observers << :party_observer
+ActiveRecord::Base.observers << :postal_address_observer
+
 #remove this plugin for the load once paths
 ActiveSupport::Dependencies.load_once_paths -= ActiveSupport::Dependencies.load_once_paths.select{|path| path =~ %r(^#{File.dirname(__FILE__)}) }
 
