1771eac0d82671e0809e0ec68775ecd33e063070
Adam H <adam@adam.olcc.lan>
2011-08-01 11:30:34 -0400

    fixed bug in desktop app generator

--- engines/erp_app/lib/generators/desktop_application/templates/migrate/migration_template.rb
+++ engines/erp_app/lib/generators/desktop_application/templates/migrate/migration_template.rb
@@ -15,6 +15,6 @@ class CreateDesktopApp<%=class_name %>
   end
 
   def self.down
-    DesktopApplication.destroy_all(:conditions => ['internal_identifier = ?','<%=file_name %>'])
+    DesktopApplication.destroy_all(['internal_identifier = ?','<%=file_name %>'])
   end
 end
