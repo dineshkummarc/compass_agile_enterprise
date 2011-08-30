3e63cbc9704cb0c6ec03f5accb9c9137afff49cc
Adam H <adam@adam.olcc.lan>
2011-08-30 14:54:32 -0400

    updating default contact us dynamic form

--- erp_forms/db/data_migrations/20110830170240_update_contact_form.rb
+++ erp_forms/db/data_migrations/20110830170240_update_contact_form.rb
@@ -1,30 +1,23 @@
-class CreateDefaultDynamicModelsAndForms
+class UpdateContactForm
   
   def self.up
-    #insert data here
-    DynamicFormModel.create(:model_name => 'DynamicFormDocument')
-
     fields = []
 
     fields << DynamicFormField.textfield({:fieldLabel => 'First Name', :name => 'first_name', :width => 250, :allowblank => false })
     fields << DynamicFormField.textfield({:fieldLabel => 'Last Name', :name => 'last_name', :width => 250, :allowblank => false })
     fields << DynamicFormField.email({:fieldLabel => 'Email', :name => 'email', :width => 250, :allowblank => false })
     fields << DynamicFormField.textarea({:fieldLabel => 'Message', :name => 'message', :width => 400, :height => 200, :allowblank => false })    
-
-    d = DynamicForm.new
-    d.description = 'Contact Form'
-    d.definition = fields.to_json
-    d.model_name = 'WebsiteInquiry'
-    d.internal_identifier = 'contact_us'
-    d.default = true
-    d.dynamic_form_model = DynamicFormModel.create(:model_name => 'WebsiteInquiry')
-    d.save
+    
+    d = DynamicForm.find_by_internal_identifier('contact_us')
+    unless d.nil?
+      puts "updating form"
+      d.definition = fields.to_json
+      d.save    
+    end    
   end
   
   def self.down
-    #remove data here
-    DynamicFormModel.delete_all
-    DynamicForm.delete_all
+    # do nothing
   end
 
 end
