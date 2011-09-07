414d475cadc2460473b8cc6e38127c29069a4b5e
Russell Holmes <russellfholmes@gmail.com>
2011-09-07 14:56:50 -0400

    updates to note_types

---
+++ engines/erp_base_erp_svcs/lib/erp_services/acts_as_note_type.rb
@@ -0,0 +1,44 @@
+module ErpServices
+  module ActsAsNoteType
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+
+    module ClassMethods
+      def acts_as_note_type
+
+        has_one :note_type_record, :as => :note_type_record, :class_name => 'NoteType'
+        
+        extend ErpServices::ActsAsNoteType
+        include ErpServices::ActsAsNoteType
+
+      end
+    end
+
+    module SingletonMethods
+    end
+
+    module InstanceMethods
+      def after_update
+        self.note_type_record.save
+      end
+
+      def after_create
+        self.note_type_record.save
+      end
+
+      def before_destroy
+        self.note_type_record.destroy
+      end
+
+      def after_initialize()
+        if (self.note_type.nil?)
+          self.note_type_record = NoteType.new
+          self.note_type_record.noted_record = self
+        end
+      end
+    end
+  end
+end
+
+ActiveRecord::Base.send(:include, ErpServices::ActsAsNoteType)
