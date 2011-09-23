d07aad133b7f7d6aae5714f1a5b9fae8f16ff1c3
Adam H <adam@adam.local>
2011-09-23 17:18:54 -0400

    turning postal_address back on cleanup

--- erp_search/lib/erp_search/engine.rb
+++ erp_search/lib/erp_search/engine.rb
@@ -6,7 +6,7 @@ module ErpSearch
     
     #add observers
 	  #this is ugly need a better way
-	  observers = [:contact_observer, :email_address_observer, :individual_observer, :organization_observer, :party_observer, :postal_address_observer]
+	  observers = [:contact_observer, :email_address_observer, :organization_observer, :party_observer, :postal_address_observer]
 	  (config.active_record.observers.nil?) ? config.active_record.observers = observers : config.active_record.observers += observers
 	  
 	  #set engine to scope
