75027a8eef0aadfdf7ce7a96884a73d529cd8463
Russell Holmes <russellfholmes@gmail.com>
2011-09-29 17:15:03 -0400

    removed test rails app

--- my_new_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-MyNewApp::Application.config.secret_token = '6406a0f0c26c98386b2805633e1582da5bf7b750de54c8f71fd069c4a4603b050d507b4a5e0ca3b7f33c130a2ab4aeb12294667900de28691e0f03450d357304'
