0bf0b3785ce6a167ea330af6f1595749c3374ae2
Ben Boyle <bboyle6681@gmail.com>
2011-10-11 15:48:42 -0400

    removed website section save.

--- engines/knitkit/app/controllers/erp_app/desktop/knitkit/website_section_controller.rb
+++ engines/knitkit/app/controllers/erp_app/desktop/knitkit/website_section_controller.rb
@@ -26,9 +26,6 @@ class ErpApp::Desktop::Knitkit::WebsiteSectionController < ErpApp::Desktop::Knit
           website.save
         else
           parent_website_section = WebsiteSection.find(params[:website_section_id])
-          website = parent_website_section.website
-          website.website_sections << website_section
-          website.save
           website_section.move_to_child_of(parent_website_section)
           parent_website_section.save
         end
