c8f385faae0a8f563ff78b0884e34cb78bb010bf
Greg Pederson <greg@nsightdevelopment.com>
2011-10-12 14:41:27 -0400

    minor organization upadte

--- engines/knitkit/lib/erp_app/widgets/gallery/base.rb
+++ engines/knitkit/lib/erp_app/widgets/gallery/base.rb
@@ -1,9 +1,5 @@
 class ErpApp::Widgets::Gallery::Base < ErpApp::Widgets::Base
 
-  def self.title
-    "Gallery"
-  end
-
   def index
     @website = Website.find_by_host(request.host_with_port)
     @images = @website.files
@@ -11,14 +7,20 @@ class ErpApp::Widgets::Gallery::Base < ErpApp::Widgets::Base
     @align = params[:align]
     render
   end
-
-  def self.name
-    File.dirname(__FILE__).split('/')[-1]
-  end
-
+  
   #if module lives outside of erp_app plugin this needs to be overriden
   #get location of this class that is being executed
   def locate
     File.dirname(__FILE__)
+  end
+  
+  class << self
+    def title
+      "Gallery"
+    end
+
+    def name
+      File.dirname(__FILE__).split('/')[-1]
+    end
   end
 end
