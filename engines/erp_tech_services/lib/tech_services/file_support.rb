1c9548eeba2747813abc90683582d8add802c537
Russell Holmes <russellfholmes@gmail.com>
2011-10-24 14:20:43 -0400

    added aws-s3 path

--- engines/erp_tech_services/lib/tech_services/file_support.rb
+++ engines/erp_tech_services/lib/tech_services/file_support.rb
@@ -1,3 +1,4 @@
 require 'tech_services/file_support/base'
 require 'tech_services/file_support/file_system_manager'
 require 'tech_services/file_support/s3_manager'
+require 'tech_services/file_support/aws_s3_patch'
---
+++ engines/erp_tech_services/lib/tech_services/file_support/aws_s3_patch.rb
@@ -0,0 +1,4 @@
+Module.class_eval do
+  alias_method :const_missing, :const_missing_not_from_s3_library
+end
+
