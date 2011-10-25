8ccbbdc954486503c573f8f961f5b96e13cdf3fa
Adam H <adam@adam.olcc.lan>
2011-10-25 09:47:21 -0400

    adding missing js functions

--- engines/erp_app/public/javascripts/erp_app/utility.js
+++ engines/erp_app/public/javascripts/erp_app/utility.js
@@ -226,3 +226,27 @@ Compass.ErpApp.Utility.JsLoader = {
     onSuccess : function() { },
     onFailure : function() { }
 }
+
+Compass.ErpApp.Utility.limitTextArea = function(textArea, limit){
+   var value = textArea.value;
+   if(value.length > limit){
+       textArea.value = value.substring(0, limit);
+   }
+   return true;
+};
+
+Compass.ErpApp.Utility.formatCurrency = function(num) {
+   num = num.toString().replace(/\$|\,/g,'');
+   if(isNaN(num))
+       num = "0";
+   var sign = (num == (num = Math.abs(num)));
+   num = Math.floor(num*100+0.50000000001);
+   var cents = num%100;
+   num = Math.floor(num/100).toString();
+   if(cents<10)
+       cents = "0" + cents;
+   for (var i = 0; i < Math.floor((num.length-(1+i))/3); i++)
+       num = num.substring(0,num.length-(4*i+3))+','+
+       num.substring(num.length-(4*i+3));
+   return (((sign)?'':'-') + '$' + num + '.' + cents);
+};
