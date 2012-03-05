f62d2720c88d0406f48008a93946fac22917840b
Chris Brodt <cbrodt@tnsolutionsinc.com>
2012-03-05 15:58:37 -0500

    Updates to compass-dev
    Added support for lib/truenorth directory

--- erp_dev_svcs/lib/erp_dev_svcs/commands/helper.rb
+++ erp_dev_svcs/lib/erp_dev_svcs/commands/helper.rb
@@ -1,9 +1,10 @@
 module ErpDevSvcs
   module Commands
     class Helper
+      COMPASS_ROOT = "lib/compass_agile_enterprise"
+      COMMERCIAL_ROOT = "lib/truenorth"
 
       def self.find_rails_root!
-        cwd = Dir.pwd
         if in_rails_application?
           return
         else
@@ -28,25 +29,32 @@ module ErpDevSvcs
       def self.exec_in_engines(only_in_these_gems = nil)
         find_rails_root!
 
-        Dir.chdir("lib/compass_agile_enterprise")
-        root_dir = Dir.pwd
+        code_dirs = [COMPASS_ROOT, COMMERCIAL_ROOT]
+        code_dirs.each do |code_dir|
+          begin
+            Dir.chdir(code_dir)
+            root_dir = Dir.pwd
 
-        #we're using gemspecs to know that we have
-        #a mountable engine located there
-        gemspecs = Dir.glob("**/*.gemspec")
-        gemspecs.each do |gem|
-          #XXX:we're skipping compass_ae since all it does is
-          #help install compass
-          next if gem == "compass_ae.gemspec"
-          gemspec = /(.*)\/(.*.gemspec)/.match(gem)
-          #set engine name to the submatch via "[1]"
-          engine_name = /(.*).gemspec/.match(gemspec[2])[1]
+            #we're using gemspecs to know that we have
+            #a mountable engine located there
+            gemspecs = Dir.glob("**/*.gemspec")
+            gemspecs.each do |gem|
+              #XXX:we're skipping compass_ae since all it does is
+              #help install compass
+              next if gem == "compass_ae.gemspec"
+              gemspec = /(.*)\/(.*.gemspec)/.match(gem)
+              #set engine name to the submatch via "[1]"
+              engine_name = /(.*).gemspec/.match(gemspec[2])[1]
 
-          if only_in_these_gems.nil? || only_in_these_gems.include?(engine_name)
-            Dir.chdir(gemspec[1])
-            #pass in the engine name
-            yield engine_name
-            Dir.chdir(root_dir)
+              if only_in_these_gems.nil? || only_in_these_gems.include?(engine_name)
+                Dir.chdir(gemspec[1])
+                #pass in the engine name
+                yield engine_name
+                Dir.chdir(root_dir)
+              end
+            end
+          rescue Errno::ENOENT => e
+            puts "#{code_dir} does not exist; skipping..."
           end
         end
       end
