62a965874889f33ff2c3e08cd130fe983748caad
Chris Brodt <chris@uberbrodt.net>
2012-03-29 10:27:46 -0400

    Had to add osg class_eval to base class

--- erp_search/app/models/party_search_fact.rb
+++ erp_search/app/models/party_search_fact.rb
@@ -2,9 +2,12 @@ class PartySearchFact < ActiveRecord::Base
   after_destroy :sunspot_commit
   
   belongs_to :party
+
+  Rails.logger.debug "########### Included BASE PartySearchFact"
   
   searchable do
     integer :party_id
+    integer :user_id
     string :eid    
     string :type
     string :roles, :multiple => true do
@@ -16,19 +19,25 @@ class PartySearchFact < ActiveRecord::Base
     text :user_login
     text :individual_current_last_name
     text :individual_current_first_name
+    string :individual_current_last_name # we need to search AND sort on first and last name
+    string :individual_current_first_name
     string :individual_current_middle_name    
     string :individual_birth_date
     text :individual_ssn    
     text :party_phone_number
     text :party_email_address    
-    string :party_address_1
-    string :party_address_2    
-    string :party_primary_address_city
-    string :party_primary_address_state    
+    string :party_email_address    
+    text :party_address_1
+    text :party_address_2    
+    text :party_primary_address_city
+    text :party_primary_address_state    
     text :party_primary_address_zip
     string :party_primary_address_country    
     string :user_enabled
-    string :user_type 
+    string :user_type
+    string :account_number, :multiple => true do
+      account_number.split(/,\s*/) unless account_number.nil?
+    end
   end
   
   def self.update_search_fact(party)    
@@ -54,6 +63,7 @@ class PartySearchFact < ActiveRecord::Base
         :party_description => party.description,
         :party_business_party_type => party.business_party_type,
         :user_login => (party.user.username rescue ''),
+        :user_id => (party.user.id rescue ''),
         :individual_current_last_name => (party.business_party.current_last_name rescue ''),
         :individual_current_first_name => (party.business_party.current_first_name rescue ''),
         :individual_current_middle_name => (party.business_party.current_middle_name rescue ''),
@@ -68,7 +78,8 @@ class PartySearchFact < ActiveRecord::Base
         :party_primary_address_zip => (party.home_postal_address.zip rescue ''),
         :party_primary_address_country => (party.home_postal_address.country rescue ''),
         :user_enabled => (party.user.enabled rescue false),
-        :user_type => (party.user.attributes['type'] rescue '')
+        :user_type => (party.user.attributes['type'] rescue ''),
+        :account_number => (BillingAccount.scoped.includes(:financial_txn_account => {:biz_txn_acct_root => :biz_txn_acct_party_roles}).where(:financial_txn_accounts => {:biz_txn_acct_roots => {:biz_txn_acct_party_roles => {:party_id => party.id }}}).map { |a| a.account_number }.join(',') rescue '')
         )
     Sunspot.commit
   end
