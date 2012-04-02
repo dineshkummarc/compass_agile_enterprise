dc623247ca17d506cc349e96dad62333a502fea8
Russell Holmes <russellfholmes@gmail.com>
2012-04-01 22:23:04 -0400

    updated loading message for confirmation look up

--- erp_app/public/javascripts/erp_app/mobile/applications/online_booking/app.js
+++ erp_app/public/javascripts/erp_app/mobile/applications/online_booking/app.js
@@ -165,31 +165,28 @@ Ext.application({
               var form = btn.up('formpanel');
               var reservation = Ext.ModelManager.getModel('Compass.ErpApp.Mobile.OnlineBooking.Reservation');
               form.setMasked({
-                xtype:'loadmask',
-                message:'Searching for reservation...'
+                xtype:'loadmask'
               });
               reservation.load(form.getValues().confirmation_number,{
                 success:function(reservation){
-                  var confirmationSearchResult = btn.up('#reservationsContainer').query('#confirmationSearchResult').first();
+                  form.setMasked(false);
+				  var mapItPanel = confirmationSearchResult.query('#mapItPanel').first();
+                  
+				  if(!Ext.isEmpty(mapItPanel)){
+                    confirmationSearchResult.remove(mapItPanel,true);
+                  }
+                  var confirmationSummary = confirmationSearchResult.query('#confirmationSummary').first();
+                  if(!Ext.isEmpty(confirmationSummary)){
+                    confirmationSearchResult.remove(confirmationSummary,true);
+                  }
+
+				  var confirmationSearchResult = btn.up('#reservationsContainer').query('#confirmationSearchResult').first();
                   if(Ext.isEmpty(reservation)){
                     confirmationSearchResult.setHtml(Compass.ErpApp.Mobile.OnlineBooking.emptyResults.apply());
                   }
                   else{
                     reservation.getAccomodationUnitType({
-                      success:function(args){
-                        form.setMasked(false);
-                        
-                        //remove mapit if its there
-                        var mapItPanel = confirmationSearchResult.query('#mapItPanel').first();
-                        if(!Ext.isEmpty(mapItPanel)){
-                          confirmationSearchResult.remove(mapItPanel,true);
-                        }
-
-                        //remove summary if its there
-                        var confirmationSummary = confirmationSearchResult.query('#confirmationSummary').first();
-                        if(!Ext.isEmpty(confirmationSummary)){
-                          confirmationSearchResult.remove(confirmationSummary,true);
-                        }
+                      success:function(args){                        
 
                         confirmationSearchResult.add({
                           xtype:'container',
@@ -288,6 +285,7 @@ Ext.application({
                         });
                       },
                       failure:function(){
+						form.setMasked(false);
                         Ext.Msg.alert("Error", "Error searching for your reservation.");
                       }
                     });
