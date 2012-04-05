46ec9047d53faec9489b089fb3c169195d85decd
Greg Pederson <greg@nsightdevelopment.com>
2012-04-05 15:56:05 -0400

    Update the Party create to find_or_create_by_description('Compass AE') in case that party already exists

--- erp_app/db/data_migrations/20120405193721_create_party_and_role_type_for_communication_events.rb
+++ erp_app/db/data_migrations/20120405193721_create_party_and_role_type_for_communication_events.rb
@@ -1,7 +1,7 @@
 class CreatePartyAndRoleTypeForCommunicationEvents < ActiveRecord::Migration
   def up
     role_type = RoleType.find_or_create_by_description_and_internal_identifier('Application', 'application')
-    party = Party.create(:description => 'Compass AE')
+    party = Party.find_or_create_by_description('Compass AE')
     party.role_type << role_type
   end
 
