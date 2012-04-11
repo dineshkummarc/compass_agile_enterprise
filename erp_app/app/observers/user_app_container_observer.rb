9c736e69d4f33119d746969f0d6db2b8d7489a4d
russ1985 <russonrails@gmail.com>
2012-04-11 14:44:44 -0400

    added mobile container creation to user observer

--- erp_app/app/observers/user_app_container_observer.rb
+++ erp_app/app/observers/user_app_container_observer.rb
@@ -5,13 +5,16 @@ class UserAppContainerObserver < ActiveRecord::Observer
     desktop = ::Desktop.create
     desktop.user = user
     desktop.setup_default_preferences
+    desktop.save
 
     organizer = Organizer.create
     organizer.user = user
     organizer.setup_default_preferences
+    organizer.save
 
     mobile = Mobile.create
     mobile.user = user
     mobile.setup_default_preferences
+    mobile.save
   end
 end
