5f3fa98513783fd03699fd7bb2804936357f881b
Adam H <adam@adam.local>
2012-04-18 12:07:42 -0400

    adding paperclip patch back in template bugfix for s3

--- erp_tech_svcs/lib/erp_tech_svcs/file_support.rb
+++ erp_tech_svcs/lib/erp_tech_svcs/file_support.rb
@@ -1,5 +1,6 @@
 require 'erp_tech_svcs/file_support/base'
 require 'erp_tech_svcs/file_support/manager'
+require 'erp_tech_svcs/file_support/paperclip_patch'
 require 'erp_tech_svcs/file_support/file_system_manager'
 require 'erp_tech_svcs/file_support/s3_manager'
 require 'erp_tech_svcs/file_support/file_manipulator'
