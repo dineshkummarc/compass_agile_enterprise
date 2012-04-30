8c549bb5cbd2e5b1c6d770718c8aa535f8da9f80
Russell Holmes <russellfholmes@gmail.com>
2012-04-30 17:16:50 -0400

    updates for relational_dynamic_attributes

--- erp_tech_svcs/lib/erp_tech_svcs/extensions/active_record/has_relational_dynamic_attributes.rb
+++ erp_tech_svcs/lib/erp_tech_svcs/extensions/active_record/has_relational_dynamic_attributes.rb
@@ -11,7 +11,7 @@ module ErpTechSvcs
           def has_relational_dynamic_attributes
             has_many :attribute_values, :as => :attributed_record, :dependent => :destroy
 
-            before_save :assign_attribute_on_save
+            before_save :assign_dynamic_attribute_on_save
 
             extend HasRelationalDynamicAttributes::SingletonMethods
             include HasRelationalDynamicAttributes::InstanceMethods
@@ -20,13 +20,13 @@ module ErpTechSvcs
         end
 
         module SingletonMethods
-          def find_by_value(value, *type_iids)
+          def find_by_dynamic_attribute(value, *type_iids)
             arel_query = AttributeValue.where('attributed_record_type = ?', self.name)
                                        .where(AttributeValue.arel_table[:value].matches("%#{value}%"))
 
             or_clauses = nil
             type_iids.each do |type_iid|
-              type = AttributeType.find_by_internal_identifier(type_iid)
+              type = AttributeType.where('description = ? or internal_identifier = ?',type_iid,type_iid).first
               raise "Attribute Type '#{type_iid}' does not exist" if type.nil?
               or_clauses = or_clauses.nil? ? AttributeValue.arel_table[:attribute_type_id].eq(type.id) : or_clauses.or(AttributeValue.arel_table[:attribute_type_id].eq(type.id))
             end
@@ -37,78 +37,55 @@ module ErpTechSvcs
         end
 
         module InstanceMethods
-          def update_or_create_attribute(value, type, data_type)
-            if self.has_attribute_value_of_type?(type)
-              update_first_attribute_value_of_type(value, type)
+          def update_or_create_dynamic_attribute(value, type, data_type)
+            if self.has_dynamic_attribute_of_type?(type)
+              update_first_dynamic_attribute_value_of_type(value, type)
             else
-              add_attribute(value, type, data_type)
+              add_dynamic_attribute(value, type, data_type)
             end
           end
 
-          def update_first_attribute_value_of_type(value, type)
+          def update_first_dynamic_attribute_value_of_type(value, type)
             attribute_type = AttributeType.find_by_internal_identifier(type)
             attribute_value = self.attribute_values.where(:attribute_type_id => attribute_type.id).first
             attribute_value.value = value
             attribute_value.save!
           end
 
-          def get_attributes
-            #template method overridden in implementing class
-            attributes_hash = {}
-            values = self.attribute_values
-            
-            values.each do |value|
-              attributes_hash[value.attribute_type.internal_identifier.to_sym] = []
+          def get_dynamic_attributes
+            {}.tap do |hash|
+              self.attribute_values.each do |value|
+                hash[value.attribute_type.description] = value.value
+              end
             end
-
-            values.each do |value|
-              attributes_hash[value.attribute_type.internal_identifier.to_sym] << value.value
-            end
-            attributes_hash
           end
 
-          def get_values_of_type(attribute_type_iid)
-            values = []
-            self.attribute_values.each do |value|
-              values << value.value if value.attribute_type.internal_identifier == attribute_type_iid
-            end
-            values
+          def get_dynamic_value_of_type(attribute_type_iid)
+            attribute_value = get_dynamic_attribute_of_type(attribute_type_iid)
+            attribute_value.nil? ? nil : attribute_value.value
           end
 
-          def get_attribute_value_records_of_type(attribute_type_iid)
-            values = []
-            self.attribute_values.each do |value|
-              values << value if value.attribute_type.internal_identifier == attribute_type_iid
-            end
-            values
+          def get_dynamic_attribute_of_type(attribute_type_iid)
+            attribute_value = self.attribute_values.includes(:attribute_type).where('attribute_types.internal_identifier = ? or attribute_types.description = ?', attribute_type_iid.to_s, attribute_type_iid.to_s).first
+            attribute_value.nil? ? nil : attribute_value
           end
           
-          def assign_attribute_on_save
+          def assign_dynamic_attribute_on_save
             #template method overridden in implementing class
           end
 
-          def has_attribute_value_of_type? (attribute_type_iid)
-            has_existing_value_of_type = false
-            self.attribute_values.each do |value|
-              if value.attribute_type.internal_identifier == attribute_type_iid
-                has_existing_value_of_type = true
-              end
-            end
-            has_existing_value_of_type
+          def has_dynamic_attribute_of_type? (attribute_type_iid)
+            !self.attribute_values.includes(:attribute_type).where('attribute_types.internal_identifier = ? or attribute_types.description = ?', attribute_type_iid.to_s, attribute_type_iid.to_s).first.nil?
           end
 
-          def destroy_values_of_type (attribute_type_iid)
-            self.attribute_values.each do |value|
-              if value.attribute_type.internal_identifier == attribute_type_iid
-                value.destroy
-              end
-            end
+          def destroy_dynamic_attribute_of_type (attribute_type_iid)
+            self.attribute_values.includes(:attribute_type).destroy_all("attribute_types.internal_identifier = #{attribute_type_iid.to_s} or attribute_types.description = #{attribute_type_iid.to_s}")
           end
 
           private
 
-          def add_attribute(value, type, data_type)
-            attribute_type = AttributeType.find_by_internal_identifier(type)
+          def add_dynamic_attribute(value, type, data_type)
+            attribute_type = AttributeType.where('description = ? or internal_identifier = ?', type).first
             attribute_type = AttributeType.create(:description => type, :data_type => data_type) unless attribute_type
             attribute_value = AttributeValue.create(:value => value, :attribute_type => attribute_type)
             self.attribute_values << attribute_value
