faa58ae1de7cab2ec9c0ce792a0754a1db4c1038
Ben Boyle <bboyle6681@gmail.com>
2012-05-03 12:00:54 -0400

    modified the dynamic extjs grid to allow for a storeId

--- erp_app/public/javascripts/erp_app/shared/dynamic_editable_grid.js
+++ erp_app/public/javascripts/erp_app/shared/dynamic_editable_grid.js
@@ -33,15 +33,27 @@ Ext.define("Compass.ErpApp.Shared.DynamicEditableGrid",{
                }
            };
         }
-
-        var store = Ext.create('Ext.data.Store', {
-            model: ((config.editable) ? config.model : undefined),
-            fields:config['fields'],
-            autoLoad: true,
-            autoSync: true,
-            pageSize: config['pageSize'],
-            proxy: config.proxy
-        });
+        if (config['storeId'] == "none"){
+          var store = Ext.create('Ext.data.Store', {
+              model: ((config.editable) ? config.model : undefined),
+              fields:config['fields'],
+              autoLoad: true,
+              autoSync: true,
+              pageSize: config['pageSize'],
+              proxy: config.proxy
+          });
+        } else {
+	      	var store = Ext.create('Ext.data.Store', {
+	              model: ((config.editable) ? config.model : undefined),
+	              fields:config['fields'],
+	              autoLoad: true,
+	              autoSync: true,
+	              pageSize: config['pageSize'],
+	              proxy: config.proxy,
+	              storeId: config['storeId'],
+	              autoLoad: true
+	          });
+        }
 
         this.store = store;
 
