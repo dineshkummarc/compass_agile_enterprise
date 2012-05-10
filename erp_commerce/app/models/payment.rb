ce5f730da6b214937eee726ecc4e3ef1fcadee4a
Russell Holmes <russellfholmes@gmail.com>
2012-05-10 15:41:22 -0400

    added destroy to finanical_txn reln

--- erp_commerce/app/models/payment.rb
+++ erp_commerce/app/models/payment.rb
@@ -1,7 +1,7 @@
 class Payment < ActiveRecord::Base
   include AASM
 
-  belongs_to :financial_txn
+  belongs_to :financial_txn, :dependent => :destroy
   has_many   :payment_gateways
 
   aasm_column :current_state
