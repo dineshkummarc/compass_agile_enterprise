1f6a0e6b1c3dd59280a7d8fbfcc985a5028a1e2b
Adam H <adam@ol7187.olcc.lan>
2012-05-14 13:01:55 -0400

    moving upgrade migration into upgrade folder

--- erp_work_effort/db/migrate/upgrade/20111216190747_add_work_effort_to_work_effort_assignment.rb
+++ erp_work_effort/db/migrate/upgrade/20111216190747_add_work_effort_to_work_effort_assignment.rb
@@ -2,6 +2,11 @@ class AddWorkEffortToWorkEffortAssignment < ActiveRecord::Migration
   def change
     unless columns(:work_effort_assignments).collect {|c| c.name}.include?('work_effort_id')
       add_column :work_effort_assignments, :work_effort_id, :integer
+
+      # migrate data
+      execute "UPDATE work_effort_assignments SET work_effort_assignments.work_effort_id=work_efforts.work_effort_assignment_id 
+               FROM work_effort_assignments JOIN work_efforts ON work_effort_assignments.id=work_efforts.work_effort_assignment_id"
+
       remove_column :work_efforts, :work_effort_assignment_id
     end
   end
