329affea80ce4020cca401041c265e8b04973ce1
Adam H <adam@ol7509.olcc.lan>
2012-05-21 15:17:33 -0400

    caching layout and contents for website section

--- knitkit/app/controllers/knitkit/website_sections_controller.rb
+++ knitkit/app/controllers/knitkit/website_sections_controller.rb
@@ -3,13 +3,31 @@ module Knitkit
 
     def index
       @current_user = current_user
-      @contents = Article.find_published_by_section(@active_publication, @website_section)
-      layout = @website_section.get_published_layout(@active_publication)
+      @contents = get_contents.dup
+      layout = get_layout.dup
+
       unless layout.nil?
         @website_section.render_base_layout? ? (render :inline => layout, :layout => 'knitkit/base') : (render :inline => layout)
       else
         @website_section.render_base_layout? ? (render) : (render :layout => false)
       end
+    end
+
+    protected
+    def get_contents
+      Rails.cache.fetch("Contents_#{@website_section.path}", :expires_in => 300.minutes) do
+        Article.find_published_by_section(@active_publication, @website_section)
+      end
+    end
+
+    def get_layout
+      Rails.cache.fetch("Layout_#{@website_section.path}", :expires_in => 300.minutes) do
+        @website_section.get_published_layout(@active_publication)
+      end
+    end
+
+    def get_cached_data(key)
+
     end
     
   end
